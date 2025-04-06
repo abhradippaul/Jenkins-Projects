4ce0e6863196a8c75ed1cb834131dadfe4e4031a
Abhradip Paul <abhradipserampore@gmail.com>
2025-04-06 22:57:38 +0530

    Change some lines in github commit deployment in shared libraries for testing purpose.

--- jenkins_shared_libraries/vars/github_commit_deployment.groovy
+++ jenkins_shared_libraries/vars/github_commit_deployment.groovy
@@ -9,10 +9,10 @@ def call(String branch, String credentialsId, String BUILD_NUMBER, String repo) 
         git config user.email "abhradipserampore@gmail.com"
         git config user.name "${username}"
         git config user.password "${password}"
-        // git checkout ${branch}
-        // git add maven_sonar_deploy
-        // git commit -m "Update deployment image to version ${BUILD_NUMBER}"
-        // git push origin ${branch} https://${password}@github.com/${username}/${repo}
+        # git checkout ${branch}
+        # git add maven_sonar_deploy
+        # git commit -m "Update deployment image to version ${BUILD_NUMBER}"
+        # git push origin ${branch} https://${password}@github.com/${username}/${repo}
         '''
     }
 }
