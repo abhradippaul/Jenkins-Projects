37159f5aeb1b76e80e5abc231972ef94e9a63c7b
Abhradip Paul <abhradipserampore@gmail.com>
2025-04-07 00:25:47 +0530

    Add some lines sixth times for testing in github commit deployment

--- jenkins_shared_libraries/vars/github_commit_deployment.groovy
+++ jenkins_shared_libraries/vars/github_commit_deployment.groovy
@@ -9,7 +9,7 @@ def call(String branch, String credentialsId, String BUILD_NUMBER, String repo) 
         git config user.email "abhradipserampore@gmail.com"
         git config user.name "${username}"
         git config user.password "${password}"
-        git url "https://github.com/abhradippaul/Jenkins-Projects.git", branch: "maven_sonar_deploy"
+        git url: "https://github.com/abhradippaul/Jenkins-Projects.git", branch: "maven_sonar_deploy"
         ls
         # git checkout maven_sonar_deploy
         # sed -i "s/replaceImageTag/${BUILD_NUMBER}/g" maven_sonar_deploy/manifests/deployment.yaml
