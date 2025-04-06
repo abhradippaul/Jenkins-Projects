0b2fd8b077a85f001ea78d863d58b1661f68fc7c
Abhradip Paul <abhradipserampore@gmail.com>
2025-04-06 23:36:52 +0530

    Fix the error second time in github commit deployment

--- jenkins_shared_libraries/vars/github_commit_deployment.groovy
+++ jenkins_shared_libraries/vars/github_commit_deployment.groovy
@@ -9,7 +9,7 @@ def call(String branch, String credentialsId, String BUILD_NUMBER, String repo) 
         git config user.email "abhradipserampore@gmail.com"
         git config user.name "${username}"
         git config user.password "${password}"
-        git checkout ${branch}
+        git checkout "${branch}"
         sed -i "s/replaceImageTag/${BUILD_NUMBER}/g" maven_sonar_deploy/manifests/deployment.yaml
         git add maven_sonar_deploy
         git commit -m "Update deployment image to version ${BUILD_NUMBER}"
