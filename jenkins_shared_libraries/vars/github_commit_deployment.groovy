842e425d0958a951ad2f57b94af83e1e30429eab
Abhradip Paul <abhradipserampore@gmail.com>
2025-04-06 23:20:53 +0530

    Update github commit deployment in shared libraries.

--- jenkins_shared_libraries/vars/github_commit_deployment.groovy
+++ jenkins_shared_libraries/vars/github_commit_deployment.groovy
@@ -9,10 +9,11 @@ def call(String branch, String credentialsId, String BUILD_NUMBER, String repo) 
         git config user.email "abhradipserampore@gmail.com"
         git config user.name "${username}"
         git config user.password "${password}"
-        # git checkout ${branch}
-        # git add maven_sonar_deploy
-        # git commit -m "Update deployment image to version ${BUILD_NUMBER}"
-        # git push origin ${branch} https://${password}@github.com/${username}/${repo}
+        git checkout ${branch}
+        sed -i "s/replaceImageTag/${BUILD_NUMBER}/g" maven_sonar_deploy/manifests/deployment.yaml
+        git add maven_sonar_deploy
+        git commit -m "Update deployment image to version ${BUILD_NUMBER}"
+        git push origin ${branch}
         '''
     }
 }
