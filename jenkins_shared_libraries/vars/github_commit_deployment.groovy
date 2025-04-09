aec21ee8df210ba06d2493934ade1efe38e93a2a
Abhradip Paul <abhradip@example.com>
2025-04-09 16:54:36 +0000

    Update the github commit deployment and fix the error

--- jenkins_shared_libraries/vars/github_commit_deployment.groovy
+++ jenkins_shared_libraries/vars/github_commit_deployment.groovy
@@ -6,20 +6,16 @@ def call(String branch, String credentialsId, String BUILD_NUMBER, String repo) 
         passwordVariable: "password"
         )]){
         git url: "https://github.com/abhradippaul/Jenkins-Projects.git", branch: "maven_sonar_deploy"
-        sh '''
-        git config user.email "abhradipserampore@gmail.com"
-        git config user.name "${username}"
-        git config user.password "${password}"
-        cd maven_sonar_deploy
-        git log --oneline 
-        git status
-        git remote -v
-        sed -i "s/replaceImageTag/${BUILD_NUMBER}/g" manifests/deployment.yaml
-        cat manifests/deployment.yaml
-        git add manifests/deployment.yaml
-        git commit -m "Update deployment image to version ${BUILD_NUMBER}"
-        git remote -v
-        git push origin maven_sonar_deploy
-        '''
+            sh '''
+                    git config user.email "abhradipserampore@gmail.com"
+                    git config user.name "${username}"
+                    cd maven_sonar_deploy
+                    git checkout maven_sonar_deploy
+                    cp template.yaml manifests/deployment.yaml
+                    sed -i "s/replaceImageTag/${BUILD_NUMBER}/g" manifests/deployment.yaml
+                    git add manifests/deployment.yaml
+                    git commit -m "Update deployment image to version ${BUILD_NUMBER}"
+                    git push https://{username}:${password}@github.com/${username}/Jenkins-Projects.git
+                    '''                  
     }
 }
